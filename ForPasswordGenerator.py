1be85be36b872970f75be8c9926e4b9f90b3c07c
Olav Henrik Hoggen <olav.hoggen@advania.no>
2023-09-28 09:38:37 +0200

    Comments

--- ForPasswordGenerator.py
+++ ForPasswordGenerator.py
@@ -4,12 +4,13 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+# Taking user input
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-
+#Loop through number inputtet, add random choice to list
 password = []
 for char in range(nr_letters):
   password+=random.choice(letters)
@@ -20,8 +21,10 @@ for char in range(nr_symbols):
 for char in range(nr_numbers):
   password+=random.choice(numbers)
 
+#Randomizing the list
 random.shuffle(password)
 
+#Converting list output to string
 separator = ''
 password = separator.join(password)
 print(f"Your generated password is: {password}")
